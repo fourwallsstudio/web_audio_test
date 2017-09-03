803bb2f3afc88f5cd7358f0ccd847d56ee00ffa3
Calvin McElroy <fourwallsstudio@gmail.com>
2017-09-03 17:32:34 -0400

    remove node_modules

--- public/main.js
+++ public/main.js
@@ -1,26 +1,7 @@
 const run = () => {
-  var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+  const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
-  // Create an empty three-second stereo buffer at the sample rate of the AudioContext
-  var myArrayBuffer = audioCtx.createBuffer(2, audioCtx.sampleRate * 3, audioCtx.sampleRate);
-
-  // Fill the buffer with white noise;
-  //just random values between -1.0 and 1.0
-  for (var channel = 0; channel < myArrayBuffer.numberOfChannels; channel++) {
-    // This gives us the actual ArrayBuffer that contains the data
-    var nowBuffering = myArrayBuffer.getChannelData(channel);
-    for (var i = 0; i < myArrayBuffer.length; i++) {
-      // Math.random() is in [0; 1.0]
-      // audio needs to be in [-1.0; 1.0]
-      nowBuffering[i] = Math.random() * 2 - 1;
-    }
-  }
-
-  var source = audioCtx.createBufferSource();
-
-  source.buffer = myArrayBuffer;
-
-  var analyser = audioCtx.createAnalyser();
+  const analyser = audioCtx.createAnalyser();
 
   const audioEl = new Audio();
   audioEl.crossOrigin = 'anonymous';
@@ -32,19 +13,15 @@ const run = () => {
   sound.connect(analyser);
 
   document.querySelector('body').append(audioEl);
-  // source.connect(analyser);
   analyser.connect(audioCtx.destination);
 
   analyser.fftSize = 2048;
-  var bufferLength = analyser.frequencyBinCount;
-  var dataArray = new Uint8Array(bufferLength);
+  const bufferLength = analyser.frequencyBinCount;
+  const dataArray = new Uint8Array(bufferLength);
   analyser.getByteTimeDomainData(dataArray);
 
-  // Get a canvas defined with ID "oscilloscope"
-  var canvas = document.getElementById("canvas");
-  var canvasCtx = canvas.getContext("2d");
-
-  // draw an oscilloscope of the current audio source
+  const canvas = document.getElementById("canvas");
+  const canvasCtx = canvas.getContext("2d");
 
   function draw() {
 
@@ -60,13 +37,13 @@ const run = () => {
 
     canvasCtx.beginPath();
 
-    var sliceWidth = canvas.width * 1.0 / bufferLength;
-    var x = 0;
+    const sliceWidth = canvas.width * 1.0 / bufferLength;
+    let x = 0;
 
     for (var i = 0; i < bufferLength; i++) {
 
-      var v = dataArray[i] / 128.0;
-      var y = v * canvas.height / 2;
+      let v = dataArray[i] / 128.0;
+      let y = v * canvas.height / 2;
 
       if (i === 0) {
         canvasCtx.moveTo(x, y);
@@ -82,8 +59,6 @@ const run = () => {
   };
 
   draw();
-
-  source.start();
 }
 
 
